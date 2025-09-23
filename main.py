bf105ce8e6db43c860f10de8d4b18d928f2ad803
batit1 <bachtuyet.barrado@gmail.com>
2025-09-23 18:52:00 +0200

    Lista edificios

---
+++ main.py
@@ -0,0 +1,44 @@
+tipos_de_edificios = [
+    {
+        "nombre": "Casa",
+        "cantidad_rango": (10, 100),
+        "coste_rango": (10000, 50000),
+        "tiempo_construccion_rango": (5, 15)
+    },
+    {
+        "nombre": "Edificio de oficinas",
+        "cantidad_rango": (2, 20),
+        "coste_rango": (100000, 500000),
+        "tiempo_construccion_rango": (30, 90)
+    },
+    {
+        "nombre": "Fábrica",
+        "cantidad_rango": (1, 10),
+        "coste_rango": (200000, 1000000),
+        "tiempo_construccion_rango": (45, 120)
+    },
+    {
+        "nombre": "Hospital",
+        "cantidad_rango": (1, 5),
+        "coste_rango": (500000, 2000000),
+        "tiempo_construccion_rango": (60, 150)
+    },
+    {
+        "nombre": "Escuela",
+        "cantidad_rango": (2, 15),
+        "coste_rango": (150000, 700000),
+        "tiempo_construccion_rango": (40, 100)
+    },
+    {
+        "nombre": "Centro comercial",
+        "cantidad_rango": (1, 5),
+        "coste_rango": (300000, 1500000),
+        "tiempo_construccion_rango": (50, 130)
+    },
+    {
+        "nombre": "Parque",
+        "cantidad_rango": (5, 30),
+        "coste_rango": (5000, 30000),
+        "tiempo_construccion_rango": (3, 10)
+    }
+]
