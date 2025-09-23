d6656e478bfe0582e3bee1878ef0fc56e7eab4e5
paula-1910 <pcortmun@myuax.com>
2025-09-23 19:11:42 +0200

    funcion crear edificios

---
+++ funciones.py
@@ -0,0 +1,16 @@
+import random
+
+def generar_edificios(tipos):
+    resultados = []
+    for edificio in tipos:
+        cantidad = random.randint(*edificio["cantidad_rango"])
+        coste = random.randint(*edificio["coste_rango"])
+        tiempo = random.randint(*edificio["tiempo_construccion_rango"])
+    
+        resultados.append({
+            "nombre": edificio["nombre"],
+            "cantidad": cantidad,
+            "coste": coste,
+            "tiempo_construccion": tiempo
+        })
+    return resultados
